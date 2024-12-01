2625aaec01f3018de6ebbcd1296734739bf85390
AlexanderWBur <abur@stpeterschools.org>
2024-12-01 16:23:50 -0600

    Setting up the moniter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/PixyBlock.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.nordicStorm;
+
+public class PixyBlock {
+    // 0, 1 0 sync (0xaa55)
+    // 2, 3 1 checksum (sum of all 16-bit words 2-6)
+    // 4, 5 2 signature number
+    // 6, 7 3 x center of object
+    // 8, 9 4 y center of object
+    // 10, 11 5 width of object
+    // 12, 13 6 height of object
+
+    // read byte : 85 read byte : -86
+    // read byte : 85 read byte : -86
+    // read byte : 22 read byte : 1
+    // read by
+    // read byte : -128 read byte : 0
+    // read byte : 118 read byte : 0
+    // read byte : 22 read byte : 0
+
+    public int sync;
+    public int checksum;
+    public int signature;
+    public int centerX;
+    public int centerY;
+    public int width;
+    public int height;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/PixyCam.java
@@ -0,0 +1,258 @@
+package org.firstinspires.ftc.teamcode.nordicStorm;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchDeviceWithParameters;
+import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
+import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.VisionProcessor;
+
+import java.util.ArrayList;
+
+
+/**
+ * @ annotations allow us to register Pixy2 as a device when configuring robot on driver hub.
+ * note that MCP9808 is in fact the XML tag for the adafruit temperature I2C Sensor. I was not able to find
+ * the appropriate tag for Pixy2 and used as shown in this demonstration 'writing an I2C Device Driver'
+ * https://github.com/FIRST-Tech-Challenge/FtcRobotController/wiki/Writing-an-I2C-Driver
+ **/
+
+@I2cDeviceType
+@DeviceProperties(name = "Pixy2 Smart Camera", description = "Pixy2 Smart Camera", xmlTag = "MCP9808")
+public class PixyCam extends I2cDeviceSynchDeviceWithParameters<I2cDeviceSynch, PixyCam.PixyCamParams> {
+
+
+    /// the default address for Pixy2s I2C interface is 0x54
+    static final I2cAddr DEFAULT_ADDRESS = I2cAddr.create7bit(0x54);
+
+
+    public ArrayList<PixyBlock> detectedBlocks = new ArrayList<>();
+
+
+    public static final int BLOCK_SIZE = 14;
+
+    /// this is the inner class used to comply with FTC SDK Architecture
+    public static class PixyCamParams implements Cloneable {
+
+        I2cAddr i2cAddr = DEFAULT_ADDRESS;
+
+        public PixyCamParams clone() {
+            try {
+                return (PixyCamParams) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException("Internal error: parameters not cloneable");
+            }
+        }
+    }
+
+    public PixyCam(I2cDeviceSynch deviceSynch) {
+        super(deviceSynch, true, new PixyCamParams());
+
+        this.deviceClient.setI2cAddress(DEFAULT_ADDRESS);
+        super.registerArmingStateCallback(false);
+        this.deviceClient.engage();
+    }
+
+    
+
+    /**
+     * @return an updated list of all "pixy block" objects registered by the camera
+     */
+    public ArrayList<PixyBlock> read() {
+
+        /// this clears the results of stale blocks
+        detectedBlocks.clear();
+        //detectedBlocks = new ArrayList<>();
+
+        /// pixy2 has 26 bytes for us to read through
+        byte[] bytes = new byte[26];
+
+        /// this sets the FTC I2C Device, in this case pixys, read window.
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(0, bytes.length, I2cDeviceSynch.ReadMode.ONLY_ONCE);
+        this.deviceClient.setReadWindow(readWindow);
+
+        ///now we read through the bytes
+        bytes = this.deviceClient.read(readWindow.getRegisterFirst(), readWindow.getRegisterCount());
+
+
+        int index = 0;
+        for (; index < bytes.length - 1; ++index) {
+            int b1 = bytes[index];
+            if (b1 < 0)
+                b1 += 256;
+
+            int b2 = bytes[index + 1];
+            if (b2 < 0)
+                b2 += 256;
+
+            if (b1 == 0x55 && b2 == 0xaa)
+                break;
+        }
+
+        if (index == 63)
+            return null;
+        else if (index == 0)
+            index += 2;
+
+
+        for (int byteOffset = index; byteOffset < bytes.length - BLOCK_SIZE - 1; ) {
+            // checking for sync block
+            int b1 = bytes[byteOffset];
+            if (b1 < 0)
+                b1 += 256;
+
+            int b2 = bytes[byteOffset + 1];
+            if (b2 < 0)
+                b2 += 256;
+
+            if (b1 == 0x55 && b2 == 0xaa) {
+                // copy block into temp buffer
+                byte[] temp = new byte[BLOCK_SIZE];
+                StringBuilder sb = new StringBuilder("Data : ");
+                for (int tempOffset = 0; tempOffset < BLOCK_SIZE; ++tempOffset) {
+                    temp[tempOffset] = bytes[byteOffset + tempOffset];
+                    sb.append(temp[tempOffset] + ", ");
+                }
+
+                PixyBlock block = bytesToBlock(temp);
+
+                //Added so blocks are only added if their signature is 1 to remove noise from signal
+                if (block.signature == 1) {
+                    detectedBlocks.add(block);
+                    byteOffset += BLOCK_SIZE - 1;
+                } else
+                    ++byteOffset;
+            } else
+                ++byteOffset;
+        }
+
+
+        if (detectedBlocks.size() >= 2) {
+            PixyBlock leftBlock;
+            PixyBlock rightBlock;
+            if (detectedBlocks.get(0).centerX > detectedBlocks.get(1).centerX) {
+                leftBlock = detectedBlocks.get(1);
+                rightBlock = detectedBlocks.get(0);
+            } else {
+                leftBlock = detectedBlocks.get(0);
+                rightBlock = detectedBlocks.get(1);
+            }
+//            double difference = (double) (rightBlock.centerX + leftBlock.centerX) / 2;
+//            setLastOffset(difference);
+//            double total = (rightBlock.centerX) - (leftBlock.centerX);
+//            getDistance(total, difference);
+
+
+//        else{
+//
+//            setLastOffset(160); //Keeps robot going straight if only one signal is picked up
+//
+
+//    } else{
+//        setLastOffset(160); //Keeps robot going straight if nothing is picked up
+//        setInRange(false);
+//    }
+
+        }
+        return detectedBlocks;
+    }
+
+    public PixyBlock bytesToBlock(byte[] bytes) {
+        PixyBlock pixyBlock = new PixyBlock();
+        pixyBlock.sync = bytesToInt(bytes[1], bytes[0]);
+        pixyBlock.checksum = bytesToInt(bytes[3], bytes[2]);
+
+
+        pixyBlock.signature = orBytes(bytes[5], bytes[4]);
+        pixyBlock.centerX = ((((int) bytes[7] & 0xff) << 8) | ((int) bytes[6] & 0xff));
+        pixyBlock.centerY = ((((int) bytes[9] & 0xff) << 8) | ((int) bytes[8] & 0xff));
+        pixyBlock.width = ((((int) bytes[11] & 0xff) << 8) | ((int) bytes[10] & 0xff));
+        pixyBlock.height = ((((int) bytes[13] & 0xff) << 8) | ((int) bytes[12] & 0xff));
+        return pixyBlock;
+    }
+
+    public int bytesToInt(int b1, int b2) {
+        if (b1 < 0)
+            b1 += 256;
+
+        if (b2 < 0)
+            b2 += 256;
+
+        int intValue = b1 * 256;
+        intValue += b2;
+        return intValue;
+    }
+
+    public int orBytes(byte b1, byte b2) {
+        return (b1 & 0xff) | (b2 & 0xff);
+    }
+
+    /// all the methods after this *besides the override ones* are total garbage
+    public byte readPixyAtByteIndex(int i) {
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(1, 26, I2cDeviceSynch.ReadMode.REPEAT);
+        this.deviceClient.setReadWindow(readWindow);
+        byte[] rawData = this.deviceClient.read(readWindow.getRegisterFirst(), readWindow.getRegisterCount());
+        return rawData[i];
+    }
+
+    public int cvt(byte lower, byte upper) {
+        return (((int) upper & 0xff) << 8) | ((int) lower & 0xff);
+    }
+
+    public byte getSpecificByte8(int b) {
+        return this.deviceClient.read8(b);
+    }
+
+    public byte[] getSpecificByte(int b) {
+        return this.deviceClient.read(b);
+    }
+
+
+    public ArrayList<Byte> readPixy() {
+        ArrayList<Byte> rawData = new ArrayList<>();
+        for (int i = 0x50; i < 0xf0; i++) {
+            rawData.add(this.deviceClient.read8(i));
+        }
+        return rawData;
+//        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(1, 26, I2cDeviceSynch.ReadMode.BALANCED);
+//        this.deviceClient.setReadWindow(readWindow);
+//        return this.deviceClient.read(readWindow.getRegisterFirst(), readWindow.getRegisterCount());
+    }
+
+    public byte[] pixyWindow() {
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(1, 26, I2cDeviceSynch.ReadMode.ONLY_ONCE);
+        this.deviceClient.setReadWindow(readWindow);
+        return this.deviceClient.read(readWindow.getRegisterFirst(), readWindow.getRegisterCount());
+
+    }
+
+    public byte[] pixyWindowDirect(int a, int b) {
+//        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(a, b, I2cDeviceSynch.ReadMode.BALANCED);
+//        this.deviceClient.setReadWindow(readWindow);
+        return this.deviceClient.read(a, b);
+
+    }
+
+
+    @Override
+    public Manufacturer getManufacturer() {
+        return Manufacturer.Other;
+    }
+
+    @Override
+    public String getDeviceName() {
+        return "pixy";
+    }
+
+    @Override
+    protected boolean internalInitialize(@NonNull PixyCam.PixyCamParams pixyCamParams) {
+        this.parameters = pixyCamParams.clone();
+        deviceClient.setI2cAddress(parameters.i2cAddr);
+
+        return true;
+    }
+}
