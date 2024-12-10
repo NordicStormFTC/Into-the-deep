3ff58f3b39e37df9ed8fd74724c4fbb1bde03bd3
AlexanderWBur <abur@stpeterschools.org>
2024-12-10 15:31:55 -0600

    Integrating gyroscope

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/AdafruitNative.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.nordicStorm;
+
+public class AdafruitNative {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/TeleOp.java
@@ -20,10 +20,14 @@ import com.acmerobotics.roadrunner.Vector2dDual;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.hardware.adafruit.AdafruitBNO055IMU;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.BNO055IMUNew;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.teamcode.Localizer;
@@ -32,6 +36,7 @@ import org.firstinspires.ftc.teamcode.nordicStorm.actionClasses.DriveActions;
 import org.firstinspires.ftc.teamcode.nordicStorm.actionClasses.LLActions;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.jetbrains.annotations.Contract;
 import org.opencv.core.Mat;
 
 import java.util.ArrayList;
@@ -41,18 +46,25 @@ import java.util.List;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends LinearOpMode {
 
+//    static {
+//        System.loadLibrary("Adafruit_BNO055");
+//    }
+
+
+    //private native int doThing();
 
     // private final VisionSubSystem.PixyActions pixy = driveTrain.getPixyActions();
     //private final VisionSubSystem.LimeLightActions limeLight = driveTrain.getLimeLightActions();
 
     private FtcDashboard dash = FtcDashboard.getInstance();
     private DriveActions drive;
-    private final Pose2d startingPos = new Pose2d(0,0,0);
+    private final Pose2d startingPos = new Pose2d(0, 0, 0);
     private LLActions llActions;
     private List<Action> runningActions = new ArrayList<>();
     private boolean doGpDrive = true;
     private AdafruitBNO055IMU externalIMU;
-   // private Arm arm;
+    BNO055IMU imu;
+    // private Arm arm;
     //    private Telemetry dashTelemetry = dash.getTelemetry();
 
 //    private List<Action> runningActions = new ArrayList<>();
@@ -61,10 +73,10 @@ public class TeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
         initIMU();
-        drive = new DriveActions(hardwareMap,startingPos,gamepad1);
+        externalIMU.startAccelerationIntegration(new Position(DistanceUnit.INCH, 0, 0, 0, System.nanoTime()), new Velocity(DistanceUnit.INCH,0,0,0,System.nanoTime()), 100);
+        drive = new DriveActions(hardwareMap, startingPos, gamepad1);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         dash.updateConfig();
-
         llActions = new LLActions(hardwareMap, startingPos, gamepad1);
         TelemetryPacket p = new TelemetryPacket();
 
@@ -74,28 +86,29 @@ public class TeleOp extends LinearOpMode {
         Pose2d pos = new Pose2d(twist.line, twist.angle);
 
         waitForStart();
+
+
         while (!isStopRequested() && opModeIsActive()) {
-           // driveTrain = new DriveTrain(hardwareMap, new Pose2d(0, 0, 0), gamepad1, gamepad2);
+            // driveTrain = new DriveTrain(hardwareMap, new Pose2d(0, 0, 0), gamepad1, gamepad2);
             p.put("pos", drive.driveBase.updatePoseEstimate().component1());
             dash.sendTelemetryPacket(p);
             dash.updateConfig();
             telemetry.update();
-
-            if(doGpDrive){
+            if (doGpDrive) {
                 runningActions.add(drive.gamepadDrive());
             }
-            if(gamepad1.a){
+            if (gamepad1.a) {
                 doGpDrive = false;
                 runningActions.add(llActions.chaseThing());
 
-            } else if(!gamepad1.a){
+            } else if (!gamepad1.a) {
                 doGpDrive = true;
             }
 
 
             List<Action> newActions = new ArrayList<>();
-            for(Action action : runningActions){
-                if(action.run(new TelemetryPacket())){
+            for (Action action : runningActions) {
+                if (action.run(new TelemetryPacket())) {
                     newActions.add(action);
                 }
             }
@@ -128,12 +141,13 @@ public class TeleOp extends LinearOpMode {
 
     }
 
-    public void initIMU(){
+    @Contract(pure = true)
+    public void initIMU() {
         externalIMU = hardwareMap.get(AdafruitBNO055IMU.class, "adafruit");
 
         BNO055IMU.Parameters p = externalIMU.getParameters();
         p.accelBandwidth = BNO055IMU.AccelBandwidth.HZ62_5;
-        p.accelRange = BNO055IMU.AccelRange.G4;
+        p.accelRange = BNO055IMU.AccelRange.G8;
         p.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         p.mode = BNO055IMU.SensorMode.ACCONLY;
 
