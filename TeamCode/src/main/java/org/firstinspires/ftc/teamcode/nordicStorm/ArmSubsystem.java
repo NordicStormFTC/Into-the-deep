51d35cb8cfdf8bae2ac34cec10f557a2f05e5705
alex <awbur@gustavus.edu>
2024-12-15 20:59:47 -0600

    goodnight pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nordicStorm/ArmSubsystem.java
@@ -3,20 +3,26 @@ package org.firstinspires.ftc.teamcode.nordicStorm;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class ArmSubsystem {
-
-    private final int UP = 2000;
-    private final int down = 0;
+/**
+ The thought here is that the vision subsystem will produce data for the
+ hardware to react to. Hence the vision subsystem extends the main robot class 'Langskip'
+ and the arm subsystem inherits the data produced by the vision subsystem. The main
+ robot class carries only the subsystems to be used in opmodes, and holds no information
+ besides one object of each subsystem.
+ */
+public class ArmSubsystem extends VisionSubsystem {
     private final DcMotor arm;
-
-    public ArmSubsystem(HardwareMap hardwareMap){
+    public ArmSubsystem(HardwareMap hardwareMap, OpType opType) {
+        super(hardwareMap, opType);
         arm = hardwareMap.get(DcMotor.class, "arm");
     }
+    public void setArmUp(double target) {
+        final double posStamp = arm.getCurrentPosition();
 
-    public void setArmUp(double target){
-        double error = target - arm.getCurrentPosition();
-        double power = 1 * error;
+        double error = target - posStamp;
+        double dError = (arm.getCurrentPosition() - posStamp) / 0.1;
+
+        double power = (1 * error) + (1 * dError);
         arm.setPower(power);
     }
-
 }
